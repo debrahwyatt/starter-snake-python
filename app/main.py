e02f9818feec27c29d60f5da9c60881215a4eb2a
debrahrwyatt@gmail.com <debrahrwyatt@gmail.com>
2019-12-10 14:07:17 -0800

    This is a test

--- app/main.py
+++ app/main.py
@@ -58,8 +58,8 @@ def move():
 
     directions = ['up', 'down', 'left', 'right']
     direction = random.choice(directions)
-
-    return move_response(direction)
+    '''"HERE"'''
+    return move_response('up')
 
 
 @bottle.post('/end')
