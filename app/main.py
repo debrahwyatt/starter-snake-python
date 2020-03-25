e940916747f138a3573fb8878ecf794e8486b0b1
UVic AI Club <57778277+uvicaiclub@users.noreply.github.com>
2020-03-24 18:05:44 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,11 +12,11 @@ direction = None
 
 @bottle.route('/')
 def index():
-	return "<h1>Dubby</h1>"
+    return "<h1>Dubby</h1>"
 
 @bottle.route('/static/<path:path>')
 def static(path):
-	return bottle.static_file(path, root='static/')
+    return bottle.static_file(path, root='static/')
 
 @bottle.post('/ping')
 def ping():
@@ -41,14 +41,12 @@ def start():
         'head_url': headUrl
     }
 
+
 @bottle.post('/move')
 def move(data=None):
 
     if not data:
         data = bottle.request.json
-
-    global direction
-
 
     # Get all the data
     you = data['you']
@@ -66,11 +64,6 @@ def move(data=None):
     food = [(f['x'], f['y']) for f in data['board']['food']]
     
     numFood = len(food)
-    
-    #moves = ["up", "down", "left", "right"]
-
-    #remove basic options, obstacles like walls, body, and other snakes.
-
 
     #possible moves with locations, remove options as required.
     moves = { "up": (you["head"][0], you["head"][1]-1),
@@ -79,38 +72,13 @@ def move(data=None):
     "right": (you["head"][0]+1, you["head"][1])
     }
 
-    temp = []
+    #Removes obstaces from standard move list
+    moves = obstacles(you["head"], moves, data["board"])
 
-    #Finds the adjacent snake body obstacles
-    for j in range(len(data["board"]["snakes"])):
-        for i in data["board"]["snakes"][j]["body"]:
-            for key in moves:
-                if (i["x"], i["y"]) == moves[key]:
-                    temp.append(key)
-    #Deletes these moves from available options
-    for i in range(len(temp)):
-        del moves[temp[i]]
 
-    #removes adjacent wall obstacles
-    if (you["head"][0]-1 < 1):
-        del moves["left"]
-    if (you["head"][0]+1 >= data['board']['width']):
-        del moves["right"]
-    if (you["head"][1]-1 < 1):
-        del moves["up"]
-    if (you["head"][1]+1 >= data['board']['height']):
-        del moves["down"]
-    
-    
-    #find closest food
-    distance_to_food = data['board']['height']*2
-    for i in data['board']["food"]:
-        temp = abs(i["x"] - you["head"][0]) + abs(i["y"] - you["head"][1])
-        if temp < distance_to_food:
-            #New closest food
-            distance_to_food = temp
-            closest_food = i
-    
+    #Fill algorithm eliminates common dead ends
+    moves = fill(you["head"], moves, data["board"])
+
 
     #Snakes that are bigger than Dubby
     bigger_snakes = []
@@ -119,11 +87,19 @@ def move(data=None):
         if len(data["board"]["snakes"][j]["body"]) >= you["size"]:
             bigger_snakes.append(data["board"]["snakes"][j]["name"])
 
+
+    #TO-DO: Algorithm for finding food
+    #       Coiling (utilize space when it's smaller than snake body)
+    #       Fight snakes
+    #       Passive mode
+
+
     #Prints relevent console information for debugging
-    print("Dubby: x =", you["head"][0], ", y =", you["head"][1])
-    print("Possible moves:", moves)
-    print("Closest food:", closest_food)
-    print("Bigger snakes:", bigger_snakes)
+    # print("Dubby: x =", you["head"][0], ", y =", you["head"][1])
+    # print("Possible moves:", moves)
+    # print("Closest food:", closest_food)
+    # print("Bigger snakes:", bigger_snakes)
+
 
     try:
         random.seed()
@@ -139,82 +115,112 @@ def move(data=None):
         'taunt': 'GG'
         }
 
-
-
-'''
-    #Avoid walls by going clockwise   
-    #Right wall avoidance
-    if you['head'][0] == walls[1]-1:
-        #Bottom right corner avoidance
-        if you['head'][1] == walls[0]-1 and direction == "down":
-            direction = "left"
-            return {
-                'move': "left"
-                }
-        else:
-            direction = "down"
-            return {
-                'move': "down"
-                }
+# Takes available moves and removes those
+# directions where space is smaller than the snake.
+# Takes the snakes initial position, moves, and board data,
+# returns the available moves
+def fill(position, moves, board):
     
-    #Bottom Wall avoidance
-    elif you['head'][1] == walls[0]-1:
-        #Bottom left corner avoidance
-        if you['head'][0] == 0 and direction == "left":
-            direction = "up"
-            return {
-                'move': "up"
-                }
-        else:
-            direction = "left"
-            return {
-                'move': "left"
-                }
+    #Generates the nodes for fill
+    all_available = []
+    for i in range(1, board["height"]):
+        for j in range(1, board["width"]):
+            all_available.append((i,j))
 
-    #Left wall avoidance
-    elif you['head'][0] == 0:
-        #Top left corner avoidance
-        if you['head'][1] == 0 and direction == "up":
-            direction = "right"
-            return {
-                'move': "right"
-                }
-        else:
-            direction = "up"
-            return {
-                'move': "up"
-                }
+    #removes obstacles for fill
+    for snake in board["snakes"]:
+        for body in snake["body"]:
+            if (body['x'], body['y']) in all_available:
+                del all_available[all_available.index((body['x'], body['y']))]
+    
+    #creates a dictionary where the directions return
+    # the size of free space available.
+    temp = {}
+    for m in moves:
+        temp[m] = len(fill_recursion(all_available, board, moves[m], [], []))
 
-    #Top wall avoidance
-    elif you['head'][1] == 0:
-        #Top right corner avoidance
-        if you['head'][0] == walls[1]-1 and direction == "right":
-            direction = "down"
-            return {
-                'move': "down"
-                }
-        else:
-            direction = "right"
-            return {
-                'move': "right"
-                }
+    #find the largest area
+    longest = 0
+    directions = []
+    for i in temp:
+        if temp[i] >= longest:
+            longest = temp[i]
 
-    elif you['head'] == walls[0]:
-        return 'move': "left"
-    elif you['head'] == walls[0]:
-        return 'move': "left"
-    elif you['head'] == walls[0]:
-        return 'move': "left"
+    #combine results which utilize the largest area
+    for i in temp:
+        if temp[i] == longest:
+            directions.append(i)
 
-    print(you['head'])
-    print(walls)
+    #Deletes moves where area is small
+    temp = []
+    for i in moves:
+        if i not in directions:
+            temp.append(i)
+    for i in temp:
+        del moves[i]
 
-    direction = "right"
-'''
+    return moves
+
+#Used exclusively in fill as the recursive portion of fill. operates on first
+#in first out.
+def fill_recursion(all_available, board, position, unexplored = [], explored = []):
+
+    #All adjacent squares
+    moves = []
+    moves.append((position[0], position[1] - 1))
+    moves.append((position[0], position[1] + 1))
+    moves.append((position[0] - 1, position[1]))
+    moves.append((position[0] + 1, position[1]))
+
+    #update the explored and unexplored portion
+    explored.append(position)
+    if position in unexplored:
+        del unexplored[unexplored.index(position)]
+        
+    #add available squares to unexplored
+    for i in moves:
+        if i not in all_available:
+            continue
+        if i in explored:
+            continue
+        if i in unexplored:
+            continue
+        unexplored.append(i)
+    
+    #Recursion as long as there are unexplored squares
+    if unexplored != []:
+        try:
+            return fill_recursion(all_available, board, unexplored[0], unexplored, explored)
+        except:
+            return explored
+
+    return explored
 
 
+#takes a position and returns the available adjacent spaces.
+def obstacles(position, moves, board):
+    if (position[0] - 1 < 1):
+        del moves["left"]
+    if (position[0] + 1 >= board['width']):
+        del moves["right"]
+    if (position[1] - 1 < 1):
+        del moves["up"]
+    if (position[1] + 1 >= board['height']):
+        del moves["down"]
 
+    temp = []
 
+    #Finds the adjacent snake body obstacles
+    for j in range(len(board["snakes"])):
+        for i in board["snakes"][j]["body"]:
+            for key in moves:
+                if (i["x"], i["y"]) == moves[key]:
+                    temp.append(key)
+    #Deletes these moves from available options
+    for i in range(len(temp)):
+        del moves[temp[i]]
+
+    return moves
 
 
 # Expose WSGI app (so gunicorn can find it)
@@ -226,3 +232,5 @@ if __name__ == '__main__':
         host=os.getenv('IP', '0.0.0.0'),
         port=os.getenv('PORT', '8080'),
         debug = True)
+
+
