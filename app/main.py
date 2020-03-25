bd6f1a3e13d00c984c81da17208c043b2abb5bb0
UVic AI Club <57778277+uvicaiclub@users.noreply.github.com>
2020-03-25 14:11:47 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -59,11 +59,29 @@ def move(data=None):
 
     health = you["health"]
 
-    walls = (data['board']['width'], data['board']['height'])
-
     food = [(f['x'], f['y']) for f in data['board']['food']]
     
     numFood = len(food)
+
+    board = data['board']
+
+    snakes = data["board"]["snakes"]
+
+    us = (you["head"][0], you["head"][1])
+
+    hungry = False
+    #Locates snake tails
+    tails = []
+    for i in snakes:
+        tails.append((i["body"][-1]['x'],i["body"][-1]['y']))
+
+    #Locates snake heads
+    heads = []
+    for i in snakes:
+        heads.append((i["body"][0]['x'],i["body"][0]['y']))
+
+    if health < 15:
+        hungry = True
 
     #possible moves with locations, remove options as required.
     moves = { "up": (you["head"][0], you["head"][1]-1),
@@ -80,40 +98,247 @@ def move(data=None):
     moves = fill(you["head"], moves, data["board"])
 
 
+    #Find and eat food
+    closest_food = closestFood(you, board, hungry)
+
+    prefered_moves = []
+
+    if closest_food != {}:
+        #left and right
+        if you["head"][0] < closest_food["x"]:
+            prefered_moves.append("right")
+        if you["head"][0] > closest_food['x']:
+            prefered_moves.append("left")
+        if you["head"][1] > closest_food['y']:
+            prefered_moves.append("up")
+        if you["head"][1] < closest_food['y']:
+            prefered_moves.append("down")
+
+
+    #Flee bigger snakes
     #Snakes that are bigger than Dubby
     bigger_snakes = []
-    for j in range(len(data["board"]["snakes"])):
-        if data["board"]["snakes"][j]["name"] == "dubby": continue
-        if len(data["board"]["snakes"][j]["body"]) >= you["size"]:
-            bigger_snakes.append(data["board"]["snakes"][j]["name"])
+    for j in range(len(snakes)):
+        if len(snakes[j]["body"]) >= you["size"]:
+            bigger_snakes.append(snakes[j]["name"])
 
 
-    #TO-DO: Algorithm for finding food
+    #Attack snake 
+    for i in snakes:
+        if i["name"] not in bigger_snakes:
+            enemy = (i['body'][0]['x'], i['body'][0]['y'])
+            us = (you["head"][0], you["head"][1])
+            
+            if enemy == (us[0], us[1] - 2):
+                prefered_moves.append("up")
+            if enemy == (us[0], us[1] + 2):
+                prefered_moves.append("down")
+            if enemy == (us[0] - 2, us[1]):
+                prefered_moves.append("left")
+            if enemy == (us[0] + 2, us[1]):
+                prefered_moves.append("right")
+
+            if enemy == (us[0] + 1, us[1] + 1):
+                prefered_moves.append("down")
+                prefered_moves.append("right")
+            if enemy == (us[0] - 1, us[1] + 1):
+                prefered_moves.append("down")
+                prefered_moves.append("left")
+            if enemy == (us[0] - 1, us[1] - 1):
+                prefered_moves.append("left")
+                prefered_moves.append("up")
+            if enemy == (us[0] + 1, us[1] - 1):
+                prefered_moves.append("up")
+                prefered_moves.append("right")
+    
+    #Removes duplicates
+    prefered_moves = list(set(prefered_moves))
+
+
+
+    dangerous_moves = []
+
+    #Avoid dangerous moves 
+    for i in snakes:
+        if i["name"] in bigger_snakes:
+            enemy = (i['body'][0]['x'], i['body'][0]['y'])
+            
+            if enemy == (us[0], us[1] - 2):
+                dangerous_moves.append("up")
+            if enemy == (us[0], us[1] + 2):
+                dangerous_moves.append("down")
+            if enemy == (us[0] - 2, us[1]):
+                dangerous_moves.append("left")
+            if enemy == (us[0] + 2, us[1]):
+                dangerous_moves.append("right")
+
+            if enemy == (us[0] + 1, us[1] + 1):
+                dangerous_moves.append("down")
+                dangerous_moves.append("right")
+            if enemy == (us[0] - 1, us[1] + 1):
+                dangerous_moves.append("down")
+                dangerous_moves.append("left")
+            if enemy == (us[0] - 1, us[1] - 1):
+                dangerous_moves.append("left")
+                dangerous_moves.append("up")
+            if enemy == (us[0] + 1, us[1] - 1):
+                dangerous_moves.append("up")
+                dangerous_moves.append("right")
+    
+    #wall adjacent squares are dangerous
+    if hungry == False:
+        if us[0] == 1:
+            dangerous_moves.append("up")
+            dangerous_moves.append("down")
+
+        if us[0] == board['width'] :
+            dangerous_moves.append("up")
+            dangerous_moves.append("down")
+
+        if us[1] == 1:
+            dangerous_moves.append("left")
+            dangerous_moves.append("right")
+
+        if us[1] == board['height']:
+            dangerous_moves.append("left")
+            dangerous_moves.append("right")
+
+
+        if us[0] == 2:
+            dangerous_moves.append("left")
+
+        if us[0] == board['width'] - 2:
+            dangerous_moves.append("right")
+
+        if us[1] == 2:
+            dangerous_moves.append("up")
+
+        if us[1] == board['height'] - 2:
+            dangerous_moves.append("down")
+
+    #Removes duplicates
+    dangerous_moves = list(set(dangerous_moves))
+
+    #merges prefered moves with all moves
+    temp = []
+    for i in moves.keys():
+        if i in prefered_moves:
+            temp.append(i)
+    prefered_moves = temp
+
+    #Risky moves that are viable
+    riskey_moves = []
+    for i in dangerous_moves:
+        if i in moves.keys():
+            riskey_moves.append(i)
+
+    #Remove moves which are dangerous
+    for i in dangerous_moves:
+        if i in moves:
+            del moves[i]
+        if i in prefered_moves:
+            del prefered_moves[prefered_moves.index(i)]
+    
+
+    if health == 1:
+        print("health")
+    #TO-DO: 
     #       Coiling (utilize space when it's smaller than snake body)
-    #       Fight snakes
     #       Passive mode
 
-
-    #Prints relevent console information for debugging
-    # print("Dubby: x =", you["head"][0], ", y =", you["head"][1])
-    # print("Possible moves:", moves)
-    # print("Closest food:", closest_food)
-    # print("Bigger snakes:", bigger_snakes)
-
-
+    #Trys the prefered move set first
     try:
         random.seed()
-        rand = random.choice(list(moves.keys()))
+        rand = random.choice(prefered_moves)
+        #print("prefered", rand)
         return {
         'move': rand,
         'taunt': 'A simple taunt'
         }
-    #No possible moves remaining, good game.
+    #If no prefered moves are feasible, any move is good.
     except:
-        return{
-        'move': "up",
-        'taunt': 'GG'
-        }
+        try:
+            random.seed()
+            rand = random.choice(list(moves.keys()))
+            #print("move", rand)
+            return {
+            'move': rand,
+            'taunt': 'A simple taunt'
+            }
+        #No possible moves remaining, crash into tails or heads, good game.
+        except:
+
+            try:
+                random.seed()
+                rand = random.choice(riskey_moves)
+                #print("risky", rand)
+                return {
+                'move': rand,
+                'taunt': 'A simple taunt'
+                }
+            except:
+                move_t = []
+                move_h = []
+
+                #Crash into tail if possible
+                if (us[0] + 1, us[1]) in tails:
+                    move_t.append("right")
+                if (us[0] - 1, us[1]) in tails:
+                    move_t.append("left")
+                if (us[0], us[1] - 1) in tails:
+                    move_t.append("up")
+                if (us[0], us[1] + 1) in tails:
+                    move_t.append("down")          
+
+                if (us[0] + 1, us[1]) in heads:
+                    move_h.append("right")
+                if (us[0] - 1, us[1]) in heads:
+                    move_h.append("left")
+                if (us[0], us[1] - 1) in heads:
+                    move_h.append("up")
+                if (us[0], us[1] + 1) in heads:
+                    move_h.append("down") 
+
+                if move_t != []:
+                    random.seed()
+                    rand = random.choice(move_t)
+                    return {
+                    'move': rand,
+                    'taunt': 'A simple taunt'
+                    }
+
+                if move_h != []:
+                    random.seed()
+                    rand = random.choice(move_h)
+                    return {
+                    'move': rand,
+                    'taunt': 'A simple taunt'
+                    }
+
+                return{
+                'move': "up",
+                'taunt': 'GG'
+                }
+
+#find the closest food
+def closestFood(you, board, hungry):
+    closest_food = {}
+    distance_to_food = board['height']*2
+    for i in board["food"]:
+        #Avoid food along the edges
+        if i['x'] > 1 and i['x'] < board['width'] - 1 and i['y'] > 1 and i['y'] < board['height'] - 1 and hungry == False:
+            temp = abs(i["x"] - you["head"][0]) + abs(i["y"] - you["head"][1])
+            if temp < distance_to_food:
+                #New closest food
+                distance_to_food = temp
+                closest_food = i
+        if hungry == True:
+            temp = abs(i["x"] - you["head"][0]) + abs(i["y"] - you["head"][1])
+            if temp < distance_to_food:
+                #New closest food
+                distance_to_food = temp
+                closest_food = i
+    return closest_food
 
 # Takes available moves and removes those
 # directions where space is smaller than the snake.
